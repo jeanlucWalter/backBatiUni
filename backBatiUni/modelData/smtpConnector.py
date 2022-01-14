f5f603d7bdb65bb09e101679d2c269a2a93cf693
jeanluc <jeanluc.walter@fantasiapp.com>
2022-01-14 16:38:07 +0100

    start smtp server test

--- backBatiUni/modelData/smtpConnector.py
+++ backBatiUni/modelData/smtpConnector.py
@@ -14,7 +14,7 @@ class SmtpConnector:
 
   def register(self, firstName, lastName, email):
     params = {"action":"mailConfirmation", "firstName":firstName, "lastName":lastName, "mail":email}
-    print("register params", params)
+    print("register params", params, self.url)
     try:
       response = requests.get(url=self.url, headers=self.headers, params=params)
       data = json.loads(response.text)

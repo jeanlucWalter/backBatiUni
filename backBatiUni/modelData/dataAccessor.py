b91600be11d05ebeb1bc2cab31748cc7c025b7b6
jeanluc <jeanluc.walter@fantasiapp.com>
2022-01-21 19:08:21 +0100

    getEnterpriseDataFrom 3

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -170,7 +170,7 @@ class DataAccessor():
     siret = request.GET["siret"] if "siret" in request.GET else None
     if os.getenv('PATH_MIDDLE'):
       print(getEnterpriseDataFrom(subName=subName, siret=siret))
-      response = {"getEnterpriseDataFrom":"OK"}.update(getEnterpriseDataFrom(subName=subName, siret=siret)["data"])
+      response = {"getEnterpriseDataFrom":"OK"}.update(getEnterpriseDataFrom(subName=subName, siret=siret))
       return response
     else:
       return {"getEnterpriseDataFrom":"Error", "messages":{"local":"no installation"}}

f33b4440dc8b3d8a2befea642360a2384c7cf61a
jeanluc <jeanluc.walter@fantasiapp.com>
2022-01-24 11:26:27 +0100

    test profileScraping 4

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -14,8 +14,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 if os.getenv('PATH_MIDDLE'):
-  print("PATH_MIDDLE", sys.path)
   sys.path.append(os.getenv('PATH_MIDDLE'))
+  print("PATH_MIDDLE", os.getcwd(), sys.path)
   from profileScraping import getEnterpriseDataFrom
 
 import json

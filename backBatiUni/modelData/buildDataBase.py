d6017d553f3761aa636e9c036c4542f8cb37fa52
jeanluc <jeanluc.walter@fantasiapp.com>
2022-02-04 12:26:28 +0100

    remove old files

--- backBatiUni/modelData/buildDataBase.py
+++ backBatiUni/modelData/buildDataBase.py
@@ -199,6 +199,11 @@ class CreateNewDataBase:
     return self.fillupDataBase(answer)
 
   def emptyDataBase (self):
+    for dir in ['./files/admin/', './files/avatars/', './files/labels/', './files/posts/']:
+      print(dir, os.listdir(dir))
+      for file in os.listdir(dir):
+        print("file :", os.path.join(dir, file))
+        os.remove(os.path.join(dir, file))
     for table in CreateNewDataBase.listTable.values():
       table.objects.all().delete()
       tableName = table.objects.model._meta.db_table
